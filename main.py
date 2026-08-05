d190474fa38987bde282ef08b8ff74d7ba3b28fc
Maurice Lorenz <me@mauricelorenz.com>
2026-08-05 04:32:02 +0200

    Add main.py, parser.py and visualizer.py and begin implementation of parsing

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+from parser import parse
+
+
+def main() -> None:
+    if len(sys.argv) >= 2:
+        path = Path(sys.argv[1])
+    else:
+        path = Path("maps/easy/01_linear_path.txt")
+    parse(path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ parser.py
@@ -0,0 +1,41 @@
+from enum import Enum
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Tuple, Any
+
+
+class Zone(Enum):
+    NORMAL = 1
+    BLOCKED = 2
+    RESTRICTED = 3
+    PRIORITY = 4
+
+
+class Drone:
+    def __init__(self, drone_id: int) -> None:
+        self.drone_id = drone_id
+
+
+@dataclass
+class Hub:
+    name: str
+    pos_x: int
+    pos_y: int
+    is_start: bool
+    is_end: bool
+    zone: Zone = Zone.NORMAL
+    color: str | None = None
+    max_drones: int = 1
+
+
+def get_input_list(path: Path) -> List[Tuple[int, str]]:
+    result = []
+    with open(path) as f:
+        for number, line in enumerate(f, 1):
+            if not line.startswith("#") and line.strip():
+                result.append((number, line.strip()))
+    return result
+
+
+def parse(path: Path) -> Any:
+    input_list = get_input_list(path) # noqa
