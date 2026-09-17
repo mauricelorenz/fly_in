d7950e3aba7df5a39984463547c4466936712c72
Maurice Lorenz <me@mauricelorenz.com>
2026-09-17 19:00:28 +0200

    Reorder attribute assignment in ParsingError

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,7 @@
 class ParsingError(Exception):
     def __init__(self, message: str, line_number: int | None = None) -> None:
-        self.line_number = line_number
         self.message = message
+        self.line_number = line_number
         show_line = f" in line {self.line_number}" if self.line_number else ""
         super().__init__(f"ParsingError{show_line}: {self.message}")
 
